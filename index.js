202381e46bc673eef1213ec911867225579e42e2
Reyes28-cyber <jonathanbim.reyes@gmail.com>
2025-04-18 18:21:25 -0500

    feat: :heavy_plus_sign: Add morgan

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const usersRouter = require("./resources/users/users.router");
 const projectsRouter = require("./resources/projects/projects.router")
-const MessageMiddleware = require("./middlewares/message.middlewares")
 
+
+const MessageMiddleware = require("./middlewares/message.middlewares")
 const app = express();
 
+
+// Middlewares
+app.use(morgan('dev'));
+
+
+
+// Routers
 app.get('/', (req, res) => {
     res.send('Servidor corriendo');
 })
 
-app.use("/users", MessageMiddleware, usersRouter);
+app.use("/users", usersRouter);
 app.use("/projects",projectsRouter)
 
 
--- resources/projects/projects.router.js
+++ resources/projects/projects.router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const { GetProjects } = require('./projects.controller');
+const MessageMiddleware = require("../../middlewares/message.middlewares")
+
 const router = express.Router();
 
 router.get('/', GetProjects)
