dbf46a0d7ea361f9596bb8e837f8df47c3eca585
Reyes28-cyber <jonathanbim.reyes@gmail.com>
2025-06-20 23:32:12 -0500

    feat: :sparkles: Get user by id

--- resources/users/users.controller.js
+++ resources/users/users.controller.js
@@ -1,10 +1,23 @@
+const HttpCodes = require("../../const/HttpCode");
+
 const GetUsers = (req, res) => {
     console.log("####### Consulatando GET USERS ######")
     res.send('GET USERS');
 };
 
 const GetUser = (req, res) => {
-    res.send('GET USERS = 1');
+
+    const { id } = req.params;
+    // dame el usuario con el id = id
+
+
+    res.status(HttpCodes.OK).json({
+    data: {
+        user: { id: id, name: `User ${id}`},
+    },
+    error: false,
+    message: "🎶 Usuario encontrado",
+    });
 };
 
 module.exports = {
